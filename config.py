7a07e5dc542efcd5ecdf2ba76f8f58ff2f978e31
sicon2002@163.com <sicon20022163.com>
2019-11-05 11:27:01 +0800

    t

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ jobCfg = {"SCHEDULER_API_ENABLED": True,
           "JOBS": [{"id": "JOB_WATER_MARK",  # 任务ID
                     "func": "ba.helpers.jobs:water_mark_handler",  # 任务位置
                     "trigger": "interval",  # 触发器
-                    "seconds": 10  # 时间间隔
+                    "seconds": 1000  # 时间间隔
                     }
-                   ]
+                ]
           }
