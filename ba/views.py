b21d583191b2eed402d342985f5be3871adefb13
sicon2002@163.com <sicon20022163.com>
2019-11-22 11:25:00 +0800

    html to image

--- ba/views.py
+++ ba/views.py
@@ -2,9 +2,18 @@
 import sys, datetime
 sys.path.append("..")
 
-from flask import render_template, session, request, url_for, redirect
+from jinja2 import PackageLoader, Environment
+import xhtml2pdf.pisa as pisa
+from cStringIO import StringIO
+from flask import render_template, session, request, url_for, redirect, current_app, make_response
 from ba.models import User
 from ba import app, sys, logic
+
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
+from xhtml2pdf.default import DEFAULT_FONT
+
+
 
 #filters
 @app.template_filter('replace')
@@ -57,6 +66,7 @@ def report(guid):
         return render_template('report/report.html', rptInfo = r[0], tskInfo = rtTaskInfo[0], teamInfo = rtTeamInfo[0], teamMembers = rtTeamMembers, tskSmps = rtTaskSamples )
     else:
         return "暂无报告，如有问题请与管理员联系！"
+
 @app.route('/report/gen/<taskid>/<userid>')
 def genReport(taskid, userid):
     rr = logic.getReportByIds(taskid, userid)
@@ -104,5 +114,51 @@ def loginAction():
         return redirect(url_for('index'))
     else:
         return redirect(url_for('login'))
+
+@app.route('/htmlpdf/<guid>', methods=['GET'])
+def htmlpdf(guid):
+    r = logic.getReportByGuid(guid)
+    if(len(r) > 0):
+        tskId = r[0]['TaskID']
+        rtTaskInfo = logic.getTaskById(tskId)
+        rtTeamInfo = logic.getTeamById(rtTaskInfo[0]['TeamID'])
+        rtTeamMembers = logic.getUsersByTeamId(rtTaskInfo[0]['TeamID'])
+        rtTaskSamples = logic.getTaskDetailByTaskId(tskId)
+
+        return render_template('report/report_pdf.html', rptInfo = r[0], tskInfo = rtTaskInfo[0], teamInfo = rtTeamInfo[0], teamMembers = rtTeamMembers, tskSmps = rtTaskSamples )
+    else:
+        return "暂无报告，如有问题请与管理员联系！"
+
+@app.route('/download/<guid>', methods=['GET'])
+def download_pdf(guid):
+    env = Environment(loader=PackageLoader(current_app.name, 'templates'))
+    template = env.get_template('report/report_pdf.html') # 获得页面模板
     
-    
+    r = logic.getReportByGuid(guid)
+    if(len(r) > 0):
+        tskId = r[0]['TaskID']
+        rtTaskInfo = logic.getTaskById(tskId)
+        rtTeamInfo = logic.getTeamById(rtTaskInfo[0]['TeamID'])
+        rtTeamMembers = logic.getUsersByTeamId(rtTaskInfo[0]['TeamID'])
+        rtTaskSamples = logic.getTaskDetailByTaskId(tskId)
+
+        fpath = current_app.root_path + '/fonts/simhei.ttf'
+        
+        html = template.render(rptInfo = r[0], tskInfo = rtTaskInfo[0], teamInfo = rtTeamInfo[0], teamMembers = rtTeamMembers, tskSmps = rtTaskSamples, font_path=fpath).encode('utf-8')
+        
+        pdfmetrics.registerFont(TTFont('yh', fpath))
+        DEFAULT_FONT['helvetica'] = 'yh'
+
+        result = StringIO()
+        pdf = pisa.CreatePDF(StringIO(html), result)
+        
+        resp = make_response(result.getvalue())
+        resp.headers["Content-Disposition"] = ("attachment; filename='{0}'; filename*=UTF-8''{0}".format('test.pdf'))
+        resp.headers['Content-Type'] = 'application/pdf'
+        return resp
+
+        # return render_template('report/report.html', rptInfo = r[0], tskInfo = rtTaskInfo[0], teamInfo = rtTeamInfo[0], teamMembers = rtTeamMembers, tskSmps = rtTaskSamples )
+    else:
+        return "暂无报告，如有问题请与管理员联系！"
+        
+
